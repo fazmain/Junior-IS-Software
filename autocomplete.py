734d47a9bd47fa7717e606b9d49336bf978d5225
fazmain <faiaz.azmain99@gmail.com>
2025-04-25 11:17:35 -0400

    Comment changes

--- autocomplete.py
+++ autocomplete.py
@@ -1,17 +1,17 @@
-# autocomplete.py
 from llama_cpp import Llama
 from huggingface_hub import hf_hub_download
 
 # Download model from Hugging Face
-# model_path = hf_hub_download(
-#     repo_id="fazmain/swiftcompose-llama-1b",
-#     filename="unsloth.Q4_K_M.gguf"  # Replace with your actual filename
-# )
-
 model_path = hf_hub_download(
-    repo_id="DevQuasar/unsloth.Llama-3.2-1B-GGUF",
-    filename="unsloth.Llama-3.2-1B.Q4_K_M.gguf"  # Replace with your actual filename
+    repo_id="fazmain/swiftcompose-llama-1b",
+    filename="unsloth.Q4_K_M.gguf"  # Replace with your actual filename
 )
+
+# Testing another model as base case
+# model_path = hf_hub_download(
+#     repo_id="DevQuasar/unsloth.Llama-3.2-1B-GGUF",
+#     filename="unsloth.Llama-3.2-1B.Q4_K_M.gguf"  # Replace with your actual filename
+# )
 
 llm = Llama(model_path=model_path, n_ctx=512)
 
--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-# server.py
 import asyncio
 import websockets
 import json
